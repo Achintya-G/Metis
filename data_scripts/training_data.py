95bf5044b1ce0262484f163bccdb0e007ddd564b
Achin <achintya.g.available@gmail.com>
2025-10-15 22:35:44 +0530

    Finished creating the program to help with recording audio. the next part looks a bit tricky time to lock in fr

--- data_scripts/training_data.py
+++ data_scripts/training_data.py
@@ -1,28 +1,36 @@
 from utils.recorder import Recorder
+from os import listdir,getcwd
+from time import sleep
 
 def record_new_word(word:str,duration:int,repetitions:int):
     r = Recorder(word)
-    for i in range(repetitions):
-        r.record_audio(duration=duration,file_name=f"{word}{i}.wav")
 
-def record_background_audio(duration:int,):
-    pass
+    for i in range(repetitions):
+        input("Hit a key to start recording.")
+        sleep(0.2)
+        
+        r.record_audio(duration=duration,file_name=f"{word}{i}.wav")
+        print(f"Progres ({i+1}/{repetitions})")
+
+def record_background_audio(duration:int):
+    r = Recorder('Background')
+
+    files = [f for f in listdir(getcwd()+'/recordings/Background/')]
+    i = 1 + len(files) # janky ahh solution but no one is ever gonna touch this :p
+
+    r.record_audio(duration=duration,file_name=f"background{i}.wav")
 
 def main():
-    print("What type of data are you going to record?")
-    print('''
-          1. New word\n
-          2. background audio\n
-          ''')
-    choice = int(input("(1 / 2 / anything other key to exit):"))
-    print(choice)
+    print("\n\nWhat type of data are you going to record?")
+    print('''1. New word\n2. background audio\n''')
+    choice = input("(1 / 2 / anything other key to exit):")
     match choice:
-        case 1:
+        case '1': 
             word = input("Enter the word : ")
             duration = int(input("Enter the duration (seconds) : "))
             repetitions = int(input("Number of times to record : "))
             record_new_word(word=word,duration=duration,repetitions=repetitions)
-        case 2:
+        case '2':
             duration = int(input("Enter the duration (seconds) : "))
             record_background_audio(duration=duration)
         case _:
